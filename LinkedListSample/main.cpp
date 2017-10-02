50067f281c2ac3be41392afb5d971596c05cd659
gnk263 <gnk263@users.noreply.github.com>
2017-10-02 20:04:13 +0900

    ユーザが入力した数値を線形リストに登録する処理を分離

--- LinkedListSample/main.cpp
+++ LinkedListSample/main.cpp
@@ -7,21 +7,29 @@
 /*--------------------------------------------*/
 
 /* ユーザが数値を入力する */
-static void InputNumberByUser();
+static void InputNumberByUser(int *inputNumber, int inputNumberSize);
 
 /* キーボード入力された数値を取得する */
 static int GetNumberByKeybord();
+
+/* ユーザが入力した数値を登録する */
+static void RegistNumber(int *registNumber, int registNumberSize);
 
 
 /*--------------------------------------------*/
 
 int main()
 {
+	int inputNumber[USER_INPUT_NUMBER_COUNT];
+
 	/* 線形リストの初期化 */
 	LinkedListInit();
 
 	/* ユーザが数値を入力する */
-	InputNumberByUser();
+	InputNumberByUser(inputNumber, USER_INPUT_NUMBER_COUNT);
+
+	/* ユーザが入力した数値を登録する */
+	RegistNumber(inputNumber, USER_INPUT_NUMBER_COUNT);
 
 	/* データを削除する */
 	LinkedListDeleteData();
@@ -34,23 +42,25 @@ int main()
 
 
 /* ユーザが数値を入力する */
-static void InputNumberByUser()
+static void InputNumberByUser(int *inputNumber, int inputNumberSize)
 {
 	int i;
-	int inputNumber;
+
+	
+	if (inputNumber == NULL || inputNumberSize <= 0)
+	{
+		return;
+	}
 
 
 	printf("数値を入力してください。\n");
 
-	for (i = 0; i < USER_INPUT_NUMBER_COUNT; i++)
+	for (i = 0; i < inputNumberSize; i++)
 	{
 		printf("> ");
 
 		/* キーボード入力された数値を取得する */
-		inputNumber = GetNumberByKeybord();
-
-		/* 線形リストにデータを追加する */
-		LinkedListAddData(inputNumber);
+		inputNumber[i] = GetNumberByKeybord();
 	}
 }
 
@@ -64,3 +74,24 @@ static int GetNumberByKeybord()
 
 	return inputNumber;
 }
+
+
+/* ユーザが入力した数値を登録する */
+static void RegistNumber(int *registNumber, int registNumberSize)
+{
+	int i;
+
+
+	if (registNumber == NULL || registNumberSize <= 0)
+	{
+		return;
+	}
+
+
+	for (i = 0; i < registNumberSize; i++)
+	{
+		/* 線形リストにデータを追加 */
+		LinkedListAddData(registNumber[i]);
+	}
+}
+
